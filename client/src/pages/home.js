57711f851d4b2f2a634b751f96cc5514edda8eec
Arielis <aetumasonis@gmail.com>
2020-10-30 22:45:20 +0100

    added style home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,10 +5,18 @@ const Home = () => {
   return (
     <>
       <main className="container home">
-        <div className="albums">
+        <div className="left">
+          <h4>CHOOSE ALBUMS TO START YOUR FAVOURITE COLLECTION:</h4>
+        </div>
+        <div className="albums"></div>
+        <div className="right">
+          <h4>GO TO COLLECTION</h4>
+          <button type="submit" className="go">
+            &gt;
+          </button>
         </div>
       </main>
-      </>
+    </>
   );
 };
 
